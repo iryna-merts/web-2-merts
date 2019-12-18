99d1b4cfe9d2072f19ecd90d35c2ba8187965d0c
iryna-merts <55079893+iryna-merts@users.noreply.github.com>
2019-12-18 04:12:23 +0200

    Manage layouts of header and footer
    Edit layout handlebar, connect bootstrap's and jquery's scripts, set the handlebars for header and footer

--- book-market/my-app/routes/index.js
+++ book-market/my-app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Web-market' });
 });
 
 module.exports = router;
